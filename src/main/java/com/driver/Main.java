354a6cfa6bbc0f9dc018228249cf4186275e9fae
Thirumoorthy Velumani <thirumoorthy@mallow-tech.com>
2023-02-08 20:51:52 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-  
+
+    public class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public class B extends A{
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public void main(String[] args){
+        B one = new B();
+        one.meth();
+        B two = new B();
+        two.meth();
+    }
 }
